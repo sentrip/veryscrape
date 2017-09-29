cff96f197d595c5c7580243b08da3ed3266bef93
djordje <djordje.m.pepic@gmail.com>
2017-09-29 20:42:47 +0200

    Added downloader and tests for downloader

---
+++ tests/test_download.py
@@ -0,0 +1,31 @@
+import unittest
+from multiprocessing import Queue
+
+from veryscrape import synchronous, Item, Producer
+from veryscrape.extensions.download import Download
+
+
+class TestDownload(unittest.TestCase):
+    url_queue = Queue()
+    result_queue = Queue()
+
+    def setUp(self):
+        self.topics = Producer.load_query_dictionary('query_topics')
+
+    @synchronous
+    async def test_download_small_url_set(self):
+        urls = ['http://fortune.com/2017/09/29/apple-mac-update-security-sierra-firmware/',
+                'https://techcrunch.com/2017/09/29/apple-quietly-acquires-computer-vision-startup-regaind/',
+                'http://www.businessinsider.com/apple-considers-ditching-intel-making-own-chip-for-mac-laptops-report-2017-9']
+        down = Download(self.url_queue, self.result_queue)
+        for u in urls:
+            self.url_queue.put(Item(u, 'AAPL', 'article'))
+        await down.stream(duration=1)
+        assert self.url_queue.empty(), 'Url queue was not purged'
+        res = self.result_queue.get_nowait()
+        assert res, 'Result queue is empty'
+        assert res.content.startswith('<'), 'Incorrect response from download'
+        await down.close()
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ veryscrape/extensions/download.py
@@ -0,0 +1,36 @@
+# Class to download urls from incoming queue and output html in outgoing queue
+import asyncio
+import re
+import time
+from contextlib import suppress
+from queue import Empty
+
+from veryscrape import Item
+from veryscrape.client import SearchClient
+
+
+class Download(SearchClient):
+    def __init__(self, url_queue, result_queue):
+        super(Download, self).__init__()
+        self.url_queue = url_queue
+        self.result_queue = result_queue
+
+    async def download(self, item):
+        # (aiohttp.Timeout, aiohttp.ClientError, ssl.CertificateError, KeyError, UnicodeDecodeError)
+        with suppress(KeyError, UnicodeDecodeError):
+            resp = await self.get(item.content, stream=True)
+            enc_search = re.search('charset=(?P<enc>\S*)', resp.headers.get('content-type', default=''))
+            encoding = enc_search.group('enc') if enc_search else 'UTF-8'
+            html_text = await resp.text(encoding=encoding)
+            if resp.status == 200:
+                self.result_queue.put(Item(html_text, item.topic, item.source))
+            resp.close()
+
+    async def stream(self, duration=0):
+        start = time.time()
+        while not duration or time.time() - start < duration or not self.url_queue.empty():
+            try:
+                url = self.url_queue.get_nowait()
+                asyncio.ensure_future(self.download(url))
+            except Empty:
+                await asyncio.sleep(0.1)
