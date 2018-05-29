b227013ccff619529063121d6e3843428b7b8e07
sentrip <djordje.m.pepic@gmail.com>
2018-05-29 16:02:10 +0200

    Refactored session

---
+++ tests/test_session.py
@@ -0,0 +1,181 @@
+import time
+import pytest
+from veryscrape.session import fetch
+
+
+@pytest.mark.asyncio
+async def test_get_attributes(patched_session):
+    async with patched_session() as sess:
+        assert len(sess.cookie_jar) == 0, \
+            "Did not get correct session attribute from _session"
+        assert sess.limiter.rate_limit_period == 60, \
+            "Did not get correct session attribute"
+
+
+@pytest.mark.asyncio
+async def test_user_agent(patched_session):
+    class Persist(patched_session):
+        user_agent = 'test'
+        persist_user_agent = True
+
+    class Change(patched_session):
+        user_agent = 'test'
+        persist_user_agent = False
+
+    for klass, cond in zip([Persist, Change], [True, False]):
+        async with klass() as sess:
+            for _ in range(2):
+                async with sess.request('GET', 'user') as resp:
+                    assert (resp == 'test') == cond, 'Incorrect user agent'
+
+
+@pytest.mark.asyncio
+async def test_proxy_request(patched_session):
+    async def g(scheme=None):
+        return type('', (), {'host': '127.0.0.1', 'port': 5000})
+    pool = type('', (), {'get': g})
+    data = []
+    async with patched_session(proxy_pool=pool) as sess:
+        async with sess.get('http://url.com', _data_collect=data) as resp:
+            pass
+    assert data[0][2]['proxy'] == 'http://127.0.0.1:5000', \
+        'Did not use correct proxy'
+
+
+@pytest.mark.asyncio
+async def test_rate_limited(assert_rate_limited, patched_session):
+    class TestRateLimitedSession(patched_session):
+        rate_limit_period = 0.1
+        rate_limits = {'/users': 5}
+
+    async with TestRateLimitedSession() as sess:
+        await assert_rate_limited(sess, 5, 'test/users', 0, 2)
+        await assert_rate_limited(sess, 5, 'test/users',
+                                  sess.rate_limit_period / sess.rate_limits['/users'], 2)
+
+
+@pytest.mark.asyncio
+async def test_rate_limited_nested_and_global(assert_rate_limited, patched_session):
+    class TestRateLimitedSession(patched_session):
+        rate_limit_period = 0.1
+        rate_limits = {'/users': {'1': 5, '2': 10}, '*': 20}
+
+    async with TestRateLimitedSession() as sess:
+        await assert_rate_limited(sess, 5, 'test/users/1', 0, 2)
+        await assert_rate_limited(sess, 5, 'test/users/1',
+                                  sess.rate_limit_period / sess.rate_limits['/users']['1'], 2)
+
+
+@pytest.mark.asyncio
+async def test_overridden_request(assert_rate_limited, patched_session):
+    calls = []
+
+    class TestRateLimitedSession(patched_session):
+        rate_limit_period = 0.01
+        rate_limits = {'/users': 5}
+
+        async def _request(self, *args, **kwargs):
+            calls.append(args)
+            return await super(TestRateLimitedSession, self)._request(*args, **kwargs)
+
+    async with TestRateLimitedSession() as sess:
+        await assert_rate_limited(sess, 5, 'test/users', 0, 2)
+        assert len(calls) == 5, 'Did not call custom request'
+        await assert_rate_limited(sess, 5, 'test/users',
+                                  sess.rate_limit_period / sess.rate_limits['/users'], 3)
+
+
+@pytest.mark.asyncio
+async def test_oauth1(patched_oauth1):
+    async with patched_oauth1('c', 's', 'tk', 'ts') as client:
+        _, params, _ = await client.patcher.patch_request('GET', 'test', {}, {})
+        required_params = [
+            'oauth_consumer_key', 'oauth_signature_method', 'oauth_nonce',
+            'oauth_timestamp', 'oauth_version', 'oauth_signature'
+        ]
+        for p in required_params:
+            assert params.get(p), 'Did not set %s in params' % p
+
+
+@pytest.mark.asyncio
+async def test_oauth1_request(patched_oauth1):
+    async with patched_oauth1('', '', '', '') as sess:
+        sess.base_url = 'http://testurl.com/'
+        res = await fetch('GET', 'stuff.html', session=sess, params={})
+        assert res.startswith('<!doctype'), \
+            'Did not correctly request with base url'
+
+
+@pytest.mark.asyncio
+async def test_oauth2_no_token(patched_oauth2):
+    """Test oauth2 token retrieval when new token is required"""
+    async with patched_oauth2(
+        'VbKYA5v77UPooA', 'OZan8kt5EluEZ0pXpMbmtLoPTgk', 'oauth2'
+    ) as client:
+        headers = await client.patcher.auth_token()
+        assert headers == {'Authorization': 'bearer ' + 'abc_test_abc'}, \
+            'Incorrect headers returned'
+
+
+@pytest.mark.asyncio
+async def test_oauth2_token_exists_not_expired(patched_oauth2):
+    """Test oauth2 token retrieval if current token is unexpired"""
+    async with patched_oauth2(
+        'VbKYA5v77UPooA', 'OZan8kt5EluEZ0pXpMbmtLoPTgk', 'oauth2'
+    ) as client:
+        client.patcher.token = 'hello'
+        client.patcher.token_expiry = time.time() + 7200
+        _, _, kwargs = await client.patcher.patch_request('', '', {}, {})
+        headers = kwargs.pop('headers')
+        assert headers == {'Authorization': 'bearer ' + 'hello'}, \
+            'Incorrect headers returned'
+
+
+@pytest.mark.asyncio
+async def test_oauth2_token_exists_expired(patched_oauth2):
+    """Test oauth2 token retrieval when current token has expired"""
+    async with patched_oauth2(
+        'VbKYA5v77UPooA', 'OZan8kt5EluEZ0pXpMbmtLoPTgk', 'oauth2'
+    ) as client:
+        client.patcher.token = 'hello'
+        client.patcher.token_expiry = time.time() - 10
+        _, _, kwargs = await client.patcher.patch_request('', '', {}, {})
+        headers = kwargs.pop('headers')
+        assert client.patcher.token != 'hello', 'Token was not updated when it should have been'
+        assert headers == {'Authorization': 'bearer ' + client.patcher.token}, \
+            'Incorrect headers returned'
+
+
+@pytest.mark.asyncio
+async def test_fetch(patched_aiohttp):
+    res = await fetch('GET', 'fetch')
+    assert res == 'test', 'Did not get correct data with fetch'
+
+
+@pytest.mark.asyncio
+async def test_fetch_stream_func(patched_aiohttp):
+    data = []
+    await fetch('GET', 'fstream', stream_func=lambda l: data.append(l))
+    assert data == ['test%d' % i for i in range(len(data))], \
+        "Did not correctly execute stream function with fetch"
+
+
+@pytest.mark.asyncio
+async def test_fetch_http_error(patched_aiohttp):
+    res = await fetch('GET', 'error')
+    assert res is '', 'Returned data for failed request'
+
+
+@pytest.mark.asyncio
+async def test_fetch_http_exception(patched_aiohttp):
+    res = await fetch('GET', 'exception')
+    assert res is '', 'Returned data for failed request'
+
+
+@pytest.mark.asyncio
+async def test_fetch_custom_session(patched_session):
+    async with patched_session() as sess:
+        res = await fetch('GET', 'fetch', session=sess)
+        assert res == 'test', 'Did not get correct data with fetch'
+        assert not sess.closed, "Closed session passed into fetch"
+
---
+++ veryscrape/session.py
@@ -0,0 +1,270 @@
+from aioauth_client import HmacSha1Signature
+from aiohttp.client import _RequestContextManager
+from collections import OrderedDict
+from fake_useragent import UserAgent, settings, FakeUserAgentError
+from hashlib import sha1
+from time import time
+from random import SystemRandom
+from urllib.parse import urljoin, urlparse
+import asyncio
+import aiohttp
+import logging
+import re
+
+
+log = logging.getLogger(__name__)
+random = SystemRandom().random
+# If fake_useragent can't connect then this allows it to finish early
+settings.HTTP_TIMEOUT = 0
+settings.HTTP_RETRIES = 0
+try:
+    _agent_factory = UserAgent()
+except FakeUserAgentError:  # pragma: nocover
+    _agent_factory = type('', (), {'random': 'no-agent'})
+
+
+def _create_nested_metadata(parent):
+    request_metadata = {}
+    for path, limit in parent.items():
+        if isinstance(limit, dict):
+            request_metadata[path] = _create_nested_metadata(limit)
+        else:
+            request_metadata[path] = {'n': 0, 'last': time()}
+    return request_metadata
+
+
+class RateLimiter:
+    def __init__(self, rate_limits, period):
+        self.rate_limit_period = period
+        self.rate_limits = OrderedDict()
+        defined_limits = rate_limits.copy()
+        global_limit = defined_limits.pop('*', None)
+        # sorts paths alphabetically for deterministic execution pre-python3.6
+        for k in sorted(list(defined_limits.keys())):
+            self.rate_limits[k] = defined_limits[k]
+        # set global rate limit last so it is always checked last
+        if global_limit:
+            assert isinstance(global_limit, int), \
+                'Global rate limit must be defined directly with an integer'
+            self.rate_limits['*'] = global_limit
+        self._rl_meta = _create_nested_metadata(self.rate_limits)
+
+    def get_limit(self, url, parent=None, metadata=None):
+        parent = parent or self.rate_limits
+        metadata = metadata or self._rl_meta
+        for path, limit in parent.items():
+            if re.search(path, url):
+                if isinstance(limit, dict):
+                    return self.get_limit(url, parent=limit,
+                                          metadata=metadata[path])
+                return limit, metadata[path]
+        else:
+            return False, {}
+
+    def refresh_limits(self, rate_limit, metadata):
+        time_since = time() - metadata['last']
+        metadata['n'] = max(0, metadata['n'] - int(
+            time_since * rate_limit / self.rate_limit_period))
+
+    async def _wait_limit(self, rate_limit, metadata):
+        while metadata['n'] >= rate_limit:
+            self.refresh_limits(rate_limit, metadata)
+            # wait 1/1000 the time of one request to try again
+            await asyncio.sleep(self.rate_limit_period / rate_limit / 1000)
+
+    async def wait_limit(self, url):
+        rate_limit, metadata = self.get_limit(url)
+        if rate_limit:
+            self.refresh_limits(rate_limit, metadata)
+            await self._wait_limit(rate_limit, metadata)
+            metadata['last'] = time()
+            metadata['n'] += 1
+
+
+class OAuth1:
+    def __init__(self, client, secret, token, token_secret):
+        self.signature = HmacSha1Signature()
+        self.client = client
+        self.secret = secret
+        self.token = token
+        self.token_secret = token_secret
+
+    @property
+    def oauth_params(self):
+        """
+        Returns dictionary of oauth1 parameters
+        required for oauth1 signed http request
+        """
+        return {
+            'oauth_consumer_key': self.client,
+            'oauth_token': self.token,
+            'oauth_signature_method': self.signature.name,
+            'oauth_nonce': sha1(str(random()).encode('ascii')).hexdigest(),
+            'oauth_timestamp': str(int(time())),
+            'oauth_version': '1.0'
+        }
+
+    async def patch_request(self, method, url, params, kwargs):
+        params.update(self.oauth_params)
+        params['oauth_signature'] = self.signature.sign(
+            self.secret, method, url, self.token_secret, **params)
+        return url, params, kwargs
+
+
+class OAuth2:
+    def __init__(self, client, secret, token_url):
+        self.client = client
+        self.secret = secret
+        self.token_url = token_url
+        self.auth = aiohttp.BasicAuth(self.client, self.secret)
+        self.token = None
+        self.token_expiry = 0
+
+    @property
+    def oauth2_token_expired(self):
+        """Returns true if current oauth2 token needs to be refreshed"""
+        return self.token is None or time() >= self.token_expiry and self.token_expiry
+
+    async def auth_token(self):
+        """
+        Returns dictionary of oauth2 parameters
+        required for oauth2 signed http request
+        """
+        if self.oauth2_token_expired:
+            async with aiohttp.ClientSession() as sess:
+                async with sess.post(
+                        self.token_url,
+                        data={'grant_type': 'client_credentials'},
+                        auth=self.auth
+                ) as resp:
+                    auth = await resp.json()
+            self.token = auth['access_token']
+            try:
+                self.token_expiry = int(time()) + int(auth['expires_in'])
+            except KeyError:
+                self.token_expiry = 0
+        return {'Authorization': 'bearer ' + self.token}
+
+    async def patch_request(self, method, url, params, kwargs):
+        auth = await self.auth_token()
+        if kwargs.get('headers', None) is None:
+            kwargs['headers'] = {}
+        kwargs['headers'].update(auth)
+        return url, params, kwargs
+
+
+class Session:
+    rate_limits = {}
+    rate_limit_period = 60
+
+    persist_user_agent = True
+    user_agent = None
+
+    def __init__(self, proxy_pool=None):
+        self.limiter = RateLimiter(self.rate_limits, self.rate_limit_period)
+        self._pool = proxy_pool
+        self._session = aiohttp.ClientSession()
+        # This is so you can call get, post, etc... without having to recode
+        # aiohttp uses _request internally for everything, so that is saved,
+        # and calls to aiohttp's _request are sent to _request of this class,
+        # which uses the saved _request at the end of the method
+        self._original_request = self._session._request
+        self._session.__dict__['_request'] = self._request
+
+    def __getattr__(self, item):
+        try:
+            return self.__getattribute__(item)
+        except AttributeError:
+            return self._session.__getattribute__(item)
+
+    async def __aenter__(self):
+        self._session = await self._session.__aenter__()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        return await self._session.__aexit__(exc_type, exc_val, exc_tb)
+
+    @property
+    def _user_agent(self):
+        """
+        Creates new random user agent, sets self.user agent and returns,
+        otherwise returns current user agent
+        """
+        if self.user_agent is None or not self.persist_user_agent:
+            self.user_agent = _agent_factory.random
+            return self.user_agent
+        else:
+            return self.user_agent
+
+    async def _request(self, method, url, **kwargs):
+        await self.limiter.wait_limit(url)
+        if kwargs.get('headers', None) is None:
+            kwargs['headers'] = {}
+        kwargs['headers'].update({'user-agent': self._user_agent})
+
+        if self._pool is not None:
+            proxy = await self._pool.get(scheme=urlparse(url).scheme)
+            kwargs.update(proxy='http://%s:%d' % (proxy.host, proxy.port))
+
+        log.debug('Requesting: \n\tMETHOD=%s, \n\tURL=%s, \n\tKWARGS=%s',
+                  method, url, str(kwargs))
+        return await self._original_request(method, url, **kwargs)
+
+    def request(self, method, url, **kwargs):
+        return _RequestContextManager(self._request(method, url, **kwargs))
+
+
+class OAuth1Session(Session):
+    base_url = None
+    _patcher = OAuth1
+
+    def __init__(self, *args, **kwargs):
+        super(OAuth1Session, self).__init__(**kwargs)
+        self.patcher = self._patcher(*args)
+
+    async def _request(self, method, url, **kwargs):
+        if self.base_url is not None and not url.startswith('http'):
+            url = urljoin(self.base_url, url)
+        url, params, kwargs = await self.patcher.patch_request(
+            method, url, kwargs.pop('params', None) or {}, kwargs
+        )
+        return await super(OAuth1Session, self)._request(
+            method, url, params=params, **kwargs
+        )
+
+
+class OAuth2Session(OAuth1Session):
+    _patcher = OAuth2
+
+
+async def fetch(method, url, *, params=None,
+                session=None, stream_func=None, **kwargs):
+    if session is None:
+        sess = aiohttp.ClientSession()
+    else:
+        sess = session
+    result = ''
+    timeout = kwargs.pop('timeout', 10)
+    kwargs.update(params=params, timeout=timeout)
+    async with sess.request(method, url, **kwargs) as resp:
+        try:
+            resp.raise_for_status()
+            if stream_func is not None:
+                async for line in resp.content:
+                    stream_func(line)
+                    await asyncio.sleep(0)
+            else:
+                result = await resp.text()
+        except aiohttp.ClientResponseError:
+            error_text = await resp.text()
+            log.error(
+                'ERROR %d requesting %-30s - %s', resp.status, url,
+                error_text[:100].replace('\n', '')
+            )
+        except Exception as e:
+            log.exception("ERROR requesting %-30s: %s", url, repr(e))
+
+    if session is None:
+        await sess.close()
+
+    return result
