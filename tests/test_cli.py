863eaba0a0a54fe4a76a278dad6c77790dc8d4ee
sentrip <djordje.m.pepic@gmail.com>
2018-05-31 02:41:37 +0200

    Fixed main cli test to be deterministic

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,15 +1,22 @@
-from threading import Timer
+from threading import Thread
 from _thread import interrupt_main
 from veryscrape.cli import main
 import pytest
+import time
 
 
 def test_main(patched_aiohttp, temp_config, patched_redis):
-    Timer(1e-1, interrupt_main).start()
+    def wait():
+        start = time.time()
+        while len(patched_redis.data['events']) < 4 and time.time() - start < 1:
+            time.sleep(1e-2)
+        interrupt_main()
+
+    Thread(target=wait).start()
     with pytest.raises(SystemExit):
         main([])
 
-    assert len(patched_redis.data['events']) == 4, 'Did not get all items'
+    assert len(patched_redis.data['events']) >= 4, 'Did not get all items'
     while len(patched_redis.data['events']):
         _, topic, _, content = patched_redis.lpop('events').split('|')
         assert topic == 'topic1', 'Incorrect topic for item'
