0aaf0a829d60bf2e0e1cfc658604caf1a5c50024
sentrip <djordje.m.pepic@gmail.com>
2018-06-03 18:05:41 +0200

    Bump version: 0.1.0 → 0.1.1

--- veryscrape/__init__.py
+++ veryscrape/__init__.py
@@ -3,7 +3,7 @@ from .veryscrape import VeryScrape, register, unregister
 
 __author__ = """Djordje Pepic"""
 __email__ = 'djordje.m.pepic@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 register('twitter', Twitter)
 register('reddit', Reddit)
